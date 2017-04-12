c53ffbf2bdd1bf3d39fa20385aec35b9116eb0f7
pedro.martins <pedro.martins@gamesys.co.uk>
2017-04-12 22:28:24 +0100

    add resources

--- winlines-web/src/components/DefaultView.js
+++ winlines-web/src/components/DefaultView.js
@@ -1,5 +1,6 @@
 import {Container,Graphics} from "pixi.js"
 import BucketView from "./bucket/view"
+import * as SPINE from 'pixi-spine';
 import ResourcesLoader from "./loader/resources-loader"
 // import WinLinesView from "./winlines/view"
 
@@ -9,6 +10,9 @@ export default class DefaultView extends Container {
         this.loaded = this.loaded.bind(this);
         const bucket = new BucketView();
         const loader = ResourcesLoader.load(this.loaded);
+        this.bucket = bucket;
+        this.resources = null;
+        this.animation = null;
         this.addChild(bucket);
 
     }
@@ -16,5 +20,36 @@ export default class DefaultView extends Container {
     loaded(ev){
       // const winlineView = new WinLinesView(ev.resources);
       // this.addChild(winlineView);
+      this.resources = ev.resources;
+      // raptor-json
+      // raptor-json
+      this.changeSpine('powerup-json');
+      //setTimeout(() => this.changeSpine('spineboy-json'), 2500);
+      //setTimeout(() => this.changeSpine('raptor-json'), 9500);
+      setTimeout(() => this.changeSpine('goblins-json'), 2500);
+    }
+
+    changeSpine(spineID){
+      let data = this.resources[spineID];
+      if(this.animation){
+        this.removeChild(this.animation);
+      }
+      this.animation = new PIXI.spine.Spine(data.spineData);
+      let animation = this.animation;
+      let element = animation.stateData.skeletonData;
+      const x = (this.bucket.width/2);
+      const y = (this.bucket.height/2)+element.height/2;
+      animation.position.set(x,y);
+      this.addChild(animation);
+      const animations = animation.state.data.skeletonData.animations;
+      let animIndex = 0;
+      animation.state.setAnimation(0, animations[0].name, false);
+      animation.state.addListener({ complete: function(track, event) {
+        animation.state.setAnimation(0, animations[animIndex].name, false);
+        animIndex++;
+        if(animIndex>=animations.length){
+          animIndex = 0;
+        }
+      }});
     }
 }
--- winlines-web/src/components/loader/resources-loader.js
+++ winlines-web/src/components/loader/resources-loader.js
@@ -8,6 +8,12 @@ export default class ResourcesLoader {
     loader.add('layout', 'res/layout.json');
     loader.add('spritesheet-json', 'res/scale-'+window.devicePixelRatio+'/sheets/high-res-sheet-0.json');
     loader.add('spritesheet-png', 'res/scale-'+window.devicePixelRatio+'/sheets/high-res-sheet-0.png');
+    //loader.add('smoulder-json', 'res/smoulder/smoulder-idle_basic.json');
+    //loader.add('mpd-json', 'res/mpd/bar-symbol-red.json');
+    loader.add('goblins-json', 'res/goblins/goblins.json');
+    loader.add('spineboy-json', 'res/spineboy/spineboy.json');
+    loader.add('raptor-json', 'res/raptor/raptor.json');
+    loader.add('powerup-json', 'res/powerup/powerup.json');
     loader.load((e)=>{
       callback.call(this,e);
     })
